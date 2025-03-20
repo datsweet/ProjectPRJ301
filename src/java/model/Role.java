b787133ef3c57fcc0052d1aea1baad4f38e385b1
DarkRainbow025 <156294494+DarkRainbow025@users.noreply.github.com>
2025-03-20 23:35:47 +0700

    Create Role.java

---
+++ src/java/model/Role.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author GIGABYTE
+ */
+public class Role {
+    private int id;
+    private String role_name;
+
+    public Role() {
+    }
+
+    public Role(int id, String role_name) {
+        this.id = id;
+        this.role_name = role_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+}
